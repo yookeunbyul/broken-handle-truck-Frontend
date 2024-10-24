e884ca3c5ddc5a06ad6770d262953d2f63077728
ppyom <ouob77@kakao.com>
2024-10-24 09:29:09 +0900

    setting: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import RootLayout from "./layouts/layout";
-import Home from "./pages/Home";
+import MainPage from "./pages/MainPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import SignUpPage from "./pages/SignUpPage.tsx";
+import MapPage from "./pages/MapPage.tsx";
+import RegisterPage from "./pages/owner/RegisterPage.tsx";
+import MyTruckPage from "./pages/owner/MyTruckPage.tsx";
+import DetailPage from "./pages/DetailPage.tsx";
+import BookMarkPage from "./pages/BookMarkPage.tsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route element={<RootLayout />}>
-          <Route path="/" element={<Home />} />
+        <Route index={true} element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<RootLayout />}>
+          <Route path="/map" element={<MapPage />} />
+          <Route path="/bookmark" element={<BookMarkPage />} />
+          <Route path="/detail" element={<DetailPage />} />
+          <Route path="/my-truck" element={<MyTruckPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
       </Routes>
     </Router>
---
+++ src/pages/BookMarkPage.tsx
@@ -0,0 +1,3 @@
+export default function BookMarkPage() {
+  return <div>Bookmark</div>;
+}
