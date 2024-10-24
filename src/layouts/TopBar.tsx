7e805b0227d95c681724b3ee0477c0b4a7208cb3
ppyom <ouob77@kakao.com>
2024-10-24 15:17:55 +0900

    design: TopBar 컴포넌트 수정
    - left 클릭 시 이전으로 이동
    - Detail 페이지에서만 버튼이 보이도록 변경
    - img 태그로 작성된 부분 svg 컴포넌트로 변경
    - BookMarkButton 컴포넌트 적용

--- src/layouts/TopBar.tsx
+++ src/layouts/TopBar.tsx
@@ -1,6 +1,6 @@
-import star from "../assets/images/star.svg";
-import leftArrow from "../assets/images/leftArrow.svg";
-import { useNavigate } from "react-router-dom";
+import LeftArrowIcon from "../assets/images/leftArrow.svg?react";
+import { useLocation, useNavigate } from "react-router-dom";
+import BookMarkButton from "../components/BookMarkButton";
 
 type TopBarProps = {
   title?: string;
@@ -8,39 +8,39 @@ type TopBarProps = {
 
 export default function TopBar({ title }: TopBarProps) {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const onClickLeft = () => {
     if (document.startViewTransition) {
       document.startViewTransition(() => {
-        navigate("/");
+        navigate(-1);
       });
     } else {
-      navigate("/");
+      navigate(-1);
     }
   };
 
   const onClickRight = () => {};
 
   return (
-    <div className="flex w-full h-12 bg-[#EF5E7A] justify-between items-center px-4 py-4 relative">
-      <div className="flex items-center gap-4">
-        <div onClick={onClickLeft}>
-          <img
-            width={20}
-            height={20}
-            src={leftArrow}
-            className="text-white cursor-pointer"
-          />
+    <div className="flex w-full bg-primary justify-center items-center px-4 py-4 sticky top-0">
+      {pathname === "/detail" && (
+        <div className="flex items-center gap-4">
+          <div onClick={onClickLeft}>
+            <LeftArrowIcon
+              width={20}
+              height={20}
+              className="fill-white cursor-pointer"
+            />
+          </div>
         </div>
-      </div>
-      <div className="absolute left-1/2 top-1/2 translate-x-1/2 translate-y-1/2">
-        {title}
-      </div>
-      <div className="flex justify-end items-center gap-spacing-05">
-        <div onClick={onClickRight} className="relative">
-          <img width={20} height={20} src={star} className="cursor-pointer" />
+      )}
+      <p className="flex-1 text-center text-xl text-white">{title}</p>
+      {pathname === "/detail" && (
+        <div className="flex justify-end items-center gap-spacing-05">
+          <BookMarkButton isBookmarked={false} onClick={onClickRight} />
         </div>
-      </div>
+      )}
     </div>
   );
 }
