e1e0df2979d582e7d850a759b75cae7cd41f9be1
yook <si89336@naver.com>
2024-10-25 15:45:59 +0900

    feat: useEffect로 setTitle 적용

--- src/pages/owner/MyTruckPage.tsx
+++ src/pages/owner/MyTruckPage.tsx
@@ -1,3 +1,18 @@
+import { useEffect } from 'react';
+import useTitleStore from '../../store/titleStore';
+import { useNavigate } from 'react-router-dom';
+
 export default function MyTruckPage() {
-  return <div>MyTruck</div>;
+    const setTitle = useTitleStore((state) => state.setTitle);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTitle('내 가게');
+    }, []);
+
+    return (
+        <div>
+            <button onClick={() => navigate(`/register`)}>내 가게 등록하기</button>
+        </div>
+    );
 }
--- src/pages/owner/RegisterPage.tsx
+++ src/pages/owner/RegisterPage.tsx
@@ -1,3 +1,12 @@
+import { useEffect } from 'react';
+import useTitleStore from '../../store/titleStore';
+
 export default function RegisterPage() {
-  return <div>Register</div>;
+    const setTitle = useTitleStore((state) => state.setTitle);
+
+    useEffect(() => {
+        setTitle('가게 등록');
+    }, []);
+
+    return <div>Register</div>;
 }
