032321a8bf463d1b501bb769606d874a172f7b36
ppyom <ouob77@kakao.com>
2024-10-28 15:25:56 +0900

    fix: Link/useNavigate를 useFadeNavigate로 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,8 @@
-import { Link } from "react-router-dom";
+import useFadeNavigate from "../hooks/useFadeNavigate.ts";
 
 const MainPage = () => {
+  const navigate = useFadeNavigate();
+
   return (
     <div className="bg-black h-full flex flex-col justify-center items-center">
       <div className="flex-[3] flex flex-col justify-center items-center gap-4">
@@ -11,19 +13,19 @@ const MainPage = () => {
         </p>
       </div>
       <div className="flex-[2] flex flex-col justify-center items-center gap-4">
-        <Link to="/map">
-          <div className="bg-primary mx-auto px-10 py-5 text-white rounded-lg font-bold sm:px-36">
-            우리 동네 푸드트럭, 지금 바로 찾아보기
-          </div>
-        </Link>
-        <Link to="/login">
-          <p className="text-white">
-            사장님이신가요?{" "}
-            <strong className="text-primary font-bold">
-              푸드트럭 등록하기
-            </strong>
-          </p>
-        </Link>
+        <div
+          className="bg-primary mx-auto px-10 py-5 text-white rounded-lg font-bold sm:px-36 cursor-pointer"
+          onClick={() => navigate("/map")}
+        >
+          우리 동네 푸드트럭, 지금 바로 찾아보기
+        </div>
+        <p
+          className="text-white cursor-pointer"
+          onClick={() => navigate("/login")}
+        >
+          사장님이신가요?{" "}
+          <strong className="text-primary font-bold">푸드트럭 등록하기</strong>
+        </p>
       </div>
     </div>
   );
