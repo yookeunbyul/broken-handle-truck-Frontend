59fee07b1258513f5cebf95046d8da8ef8f25d31
yook <si89336@naver.com>
2024-10-28 16:50:01 +0900

    fix: flex 수정 및 useFadeNavigate 설정

--- src/layouts/TopBar.tsx
+++ src/layouts/TopBar.tsx
@@ -24,20 +24,18 @@ export default function TopBar({ title }: TopBarProps) {
 
     return (
         <div className="flex w-full bg-primary justify-center items-center px-4 py-5 sticky top-0 z-10">
-            <div className="w-[calc(100%-50px)] sm:w-[calc(100%-100px)] flex">
-                {(pathname === '/detail' || pathname === '/register') && (
-                    <div className="flex items-center gap-4">
+            <div className="w-[calc(100%-50px)] sm:w-[calc(100%-100px)] flex items-center">
+                <div className="flex-1 flex items-center">
+                    {(pathname === '/detail' || pathname === '/register') && (
                         <div onClick={onClickLeft}>
-                            <LeftArrowIcon width={20} height={20} className="fill-white cursor-pointer" />
+                            <LeftArrowIcon width={25} height={25} className="fill-white cursor-pointer" />
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
                 <p className="flex-1 text-center text-lg text-white font-bold">{title}</p>
-                {pathname === '/detail' && (
-                    <div className="flex justify-end items-center gap-spacing-05">
-                        <BookMarkButton isBookmarked={false} onClick={onClickRight} size={30} />
-                    </div>
-                )}
+                <div className="flex-1 flex items-center justify-end">
+                    {pathname === '/detail' && <BookMarkButton isBookmarked={false} onClick={onClickRight} size={30} />}
+                </div>
             </div>
         </div>
     );
