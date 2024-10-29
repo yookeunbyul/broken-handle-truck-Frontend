430ba97e951c36df5322cbe85c1f609dab4e909f
yook <si89336@naver.com>
2024-10-29 16:27:14 +0900

    design: ui 수정

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,74 +1,54 @@
-import BookMarkButton from "./BookMarkButton";
-import Menu from "../assets/images/defaultMenu.svg?react";
+import BookMarkButton from './BookMarkButton';
+import Menu from '../assets/images/defaultMenu.svg?react';
 interface CardProps {
-  isOpen: boolean;
-  info: {
-    category: string;
-    name: string;
-    visited: number;
-  };
-  bg: string;
+    isOpen: boolean;
+    info: {
+        category: string;
+        name: string;
+        visited: number;
+    };
+    bg: string;
 }
-export default function Card({
-  isOpen = false,
-  info,
-  bg = "black",
-}: CardProps) {
-  const handleClick = () => {};
+export default function Card({ isOpen = false, info, bg = 'black' }: CardProps) {
+    const handleClick = () => {};
 
-  return (
-    <div
-      className={`bg-${bg} mx-auto w-[calc(100%-50px)] sm:w-full rounded-2xl p-6 flex flex-col gap-y-5 ${
-        bg === "white" ? "drop-shadow-lg" : null
-      }`}
-    >
-      <div className="tracking-tight flex gap-x-3">
-        <div className="flex-none flex items-center justify-center">
-          <Menu width={90} height={90} />
-        </div>
-        <div className="flex flex-col justify-center gap-y-2">
-          <div className="text-xs text-category">카테고리 {info.category}</div>
-          <div
-            className={`${
-              bg === "white" ? "text-black" : "text-white"
-            } font-bold text-base`}
-          >
-            이름 {info.name}
-          </div>
-          <div className="text-xs text-category bg-count px-2 py-1 rounded-2xl whitespace-nowrap">
-            최근 방문{" "}
-            <span className="text-white font-bold">{info.visited}</span>명
-          </div>
-        </div>
-        <div className="flex-1 flex flex-col gap-y-2 pt-2">
-          <div className="flex justify-end">
-            <BookMarkButton
-              isBookmarked={true}
-              onClick={handleClick}
-              size={30}
-            />
-          </div>
-          <div className="flex justify-end text-xs gap-x-1 text-white items-center">
-            <div
-              className={`w-1 h-1 rounded-full ${
-                isOpen ? "bg-success" : "bg-red-500"
-              }`}
-            ></div>
-            <div
-              className={`whitespace-nowrap ${
-                bg === "white" ? "text-black" : "text-white"
-              }`}
-            >
-              {isOpen ? "운영중" : "운영종료"}
+    return (
+        <div
+            className={`bg-${bg} mx-auto w-[calc(100%-50px)] sm:w-full rounded-2xl p-6 flex flex-col gap-y-5 ${
+                bg === 'white' ? 'drop-shadow-lg' : null
+            }`}
+        >
+            <div className="tracking-tight flex gap-x-3">
+                <div className="flex-none flex items-center justify-center">
+                    <Menu width={90} height={90} />
+                </div>
+                <div className="flex flex-col justify-center gap-y-2">
+                    <div className="text-xs text-category">카테고리 {info.category}</div>
+                    <div className={`${bg === 'white' ? 'text-black' : 'text-white'} font-bold text-base`}>
+                        이름 {info.name}
+                    </div>
+                    <div className="gap-x-1 text-xs text-category bg-count px-2 py-1 rounded-2xl whitespace-nowrap inline-flex max-w-fit">
+                        <span>최근 방문</span>
+                        <span className="text-white">{info.visited}명</span>
+                    </div>
+                </div>
+                <div className="flex-1 flex flex-col gap-y-2 pt-2">
+                    <div className="flex justify-end">
+                        <BookMarkButton isBookmarked={true} onClick={handleClick} size={30} />
+                    </div>
+                    <div className="flex justify-end text-xs gap-x-1 text-white items-center">
+                        <div className={`w-1 h-1 rounded-full ${isOpen ? 'bg-success' : 'bg-red-500'}`}></div>
+                        <div className={`whitespace-nowrap ${bg === 'white' ? 'text-black' : 'text-white'}`}>
+                            {isOpen ? '운영중' : '운영종료'}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+            <div className="text-right">
+                <button className="text-right bg-primary text-base py-2 px-3 tracking-tight rounded-md text-white font-bold">
+                    자세히 보기
+                </button>
+            </div>
         </div>
-      </div>
-      <div className="text-right">
-        <button className="text-right bg-primary text-base py-2 px-3 tracking-tight rounded-md text-white font-bold">
-          자세히 보기
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
--- src/pages/BookMarkPage.tsx
+++ src/pages/BookMarkPage.tsx
@@ -1,37 +1,38 @@
-import { useEffect } from "react";
-import useTitleStore from "../store/titleStore";
-import Card from "../components/Card";
-import Heart from "../assets/images/heart.svg?react";
-import myBookMark from "../mocks/myBookMark.json";
-import NoBookMark from "../components/bookmark/NoBookMark";
+import { useEffect } from 'react';
+import useTitleStore from '../store/titleStore';
+import Card from '../components/Card';
+import Heart from '../assets/images/heart.svg?react';
+import myBookMark from '../mocks/myBookMark.json';
+import NoBookMark from '../components/bookmark/NoBookMark';
 
 console.log(myBookMark);
 
 export default function BookMarkPage() {
-  const setTitle = useTitleStore((state) => state.setTitle);
+    const setTitle = useTitleStore((state) => state.setTitle);
 
-  useEffect(() => {
-    setTitle("북마크");
-  }, []);
+    useEffect(() => {
+        setTitle('북마크');
+    }, []);
 
-  return (
-    <div className="flex mt-4 flex-col gap-y-4 mx-auto w-[calc(100%-100px)] sm:w-[calc(100%-200px)]">
-      {myBookMark.length >= 1 ? (
-        <>
-          <p className="flex items-center gap-x-2">
-            <Heart />
-            <strong>나만의 푸드트럭</strong>
-            {myBookMark.length}개
-          </p>
-          {myBookMark.map((data) => (
-            <div key={data.id} className="flex flex-col">
-              <Card isOpen={data.isContinue} info={data} bg="white" />
-            </div>
-          ))}
-        </>
-      ) : (
-        <NoBookMark />
-      )}
-    </div>
-  );
+    return (
+        <div className="flex py-11 flex-col gap-y-8 mx-auto w-[calc(100%-100px)] sm:w-[calc(100%-200px)]">
+            {myBookMark.length >= 1 ? (
+                <>
+                    <p className="flex items-center">
+                        <Heart width={18} height={18} />
+                        <strong className="px-1 tracking-tight text-lg">나만의 푸드트럭</strong>
+                        <strong className="text-primary text-lg">{myBookMark.length}</strong>
+                        <span className="text-lg">개</span>
+                    </p>
+                    {myBookMark.map((data) => (
+                        <div key={data.id} className="flex flex-col">
+                            <Card isOpen={data.isContinue} info={data} bg="white" />
+                        </div>
+                    ))}
+                </>
+            ) : (
+                <NoBookMark />
+            )}
+        </div>
+    );
 }
