fe3ce9d0d109e0a08374fe2a1d7d09e5f22d36af
ppyom <ouob77@kakao.com>
2024-10-29 09:45:15 +0900

    fix: Select 컴포넌트에서 categories 사용하도록 수정

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,30 +1,27 @@
+import { categories } from "../constants/categories.ts";
+
 export default function Select() {
-    return (
-        <div className="flex flex-col gap-1 mx-auto w-[calc(100%-100px)] sm:w-[calc(100%-200px)]">
-            <p className="mb-2 text-xs text-white/50 mix-blend-difference tracking-tight">카테고리</p>
-            <select
-                name="menu"
-                required
-                onChange={(e) => console.log(e.target.value)}
-                className="text-base w-full border-r-8 border-transparent rounded-lg bg-form p-3"
-            >
-                <option value="" selected>
-                    카테고리
-                </option>
-                <option value="붕어빵">붕어빵</option>
-                <option value="어묵">어묵</option>
-                <option value="고구마">고구마</option>
-                <option value="호떡">호떡</option>
-                <option value="타코야끼">타코야끼</option>
-                <option value="찐빵">찐빵</option>
-                <option value="떡볶이">떡볶이</option>
-                <option value="와플">와플</option>
-                <option value="토스트">토스트</option>
-                <option value="통닭">통닭</option>
-                <option value="풀빵">풀빵</option>
-                <option value="계란빵">계란빵</option>
-                <option value="땅콩빵">땅콩빵</option>
-            </select>
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-1 mx-auto w-[calc(100%-100px)] sm:w-[calc(100%-200px)]">
+      <p className="mb-2 text-xs text-white/50 mix-blend-difference tracking-tight">
+        카테고리
+      </p>
+      <select
+        name="menu"
+        required
+        onChange={(e) => console.log(e.target.value)}
+        defaultValue=""
+        className="text-base w-full border-r-8 border-transparent rounded-lg bg-form p-3 outline-none"
+      >
+        <option value="" disabled hidden>
+          카테고리
+        </option>
+        {categories.map((category, idx) => (
+          <option key={`option_${idx}`} value={category.title}>
+            {category.title}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
