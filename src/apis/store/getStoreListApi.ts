2f028221d5a98df5200c57cdc42bb4cdaab6dda2
ppyom <ouob77@kakao.com>
2024-10-30 14:42:18 +0900

    docs: 사용자 주변 가게 정보에 주석 추가

--- src/apis/store/getStoreListApi.ts
+++ src/apis/store/getStoreListApi.ts
@@ -12,6 +12,13 @@ interface StoreListApiData {
   stores: IStore[];
 }
 
+/**
+ * 사용자 주변 가게 정보
+ * @param lon 경도(longitude)
+ * @param lat 위도(latitude)
+ * @param category **optional** 카테고리
+ * @param storeName **optional** 검색어(가게 이름)
+ */
 const getStoreList = async <T = { msg: string } & StoreListApiData>({
   lat,
   lon,
