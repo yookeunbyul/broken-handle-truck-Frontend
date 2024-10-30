351405a266f94d97f51faeea7192c20e406039cd
ppyom <ouob77@kakao.com>
2024-10-30 17:20:48 +0900

    setting: POST method의 Data 타입 변경

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -10,8 +10,8 @@ export const http = {
   get: function get<T, P = undefined>(url: string, params?: P): Promise<T> {
     return service.get(url, { params }).then((res) => res.data);
   },
-  post: function post<T, D>(url: string, data: D): Promise<T> {
-    return service.post(url, { ...data });
+  post: function post<T, D = undefined>(url: string, data?: D): Promise<T> {
+    return service.post(url, data && { ...data });
   },
   delete: function remove<T>(url: string): Promise<T> {
     return service.delete(url);
