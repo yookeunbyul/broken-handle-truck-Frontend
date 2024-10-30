0ac095591c31227ef4bb1ed33cee268c1e6116b4
ppyom <ouob77@kakao.com>
2024-10-30 14:16:36 +0900

    feat: 특정 매장 정보 조회

---
+++ src/apis/store/getStoreApi.ts
@@ -0,0 +1,20 @@
+import apiClient from "../apiClient.ts";
+import { IStore } from "../../types/store.ts";
+
+interface StoreApiData {
+  store: IStore;
+  comments: object[]; // CommentType 받은 이후 수정 필요
+}
+
+/**
+ * 특정 매장 정보
+ * @param id storeId
+ */
+const getStore = async <T = { msg: string } & StoreApiData>(
+  id: string,
+): Promise<T> => {
+  const res = await apiClient.get<T>(`/store/${id}`);
+  return res.data;
+};
+
+export { getStore };
