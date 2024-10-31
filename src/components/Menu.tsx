18cb64228b3621fdad907e3177f30e1e75c755d5
JunYoung <whwnsdud011@naver.com>
2024-10-31 09:20:42 +0900

    desgin: ~~ 수정

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,17 +1,20 @@
-import Icon from '../assets/images/defaultMenu.svg?react';
+import Icon from "../assets/images/defaultMenu.svg?react";
 
 interface MenuProps {
-    title: string;
-    ImgComponent?: React.FC<{ width?: number; height?: number }>;
+  title: string;
+  ImgComponent?: React.FC<{ width?: number; height?: number }>;
 }
 
 export default function Menu({ title, ImgComponent = Icon }: MenuProps) {
-    return (
-        <div className="inline-block">
-            <div className="flex justify-center bg-secondary rounded-full p-3">
-                <ImgComponent width={50} height={50} />
-            </div>
-            <div className="flex justify-center text-base tracking-tighter mt-2 text-white">{title}</div>
-        </div>
-    );
+  return (
+    <div className="inline-block">
+      <div className="flex justify-center bg-secondary rounded-full p-3">
+        <ImgComponent width={50} height={50} />
+      </div>
+
+      <div className="flex justify-center text-base tracking-tighter mt-2 text-white">
+        {title}
+      </div>
+    </div>
+  );
 }
