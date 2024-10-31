66d65b9344b1df2984399a8c7883633dae1bbf74
yook <si89336@naver.com>
2024-10-31 15:32:02 +0900

    design: 북마크 ui 수정

--- src/components/bookmark/NoBookMark.tsx
+++ src/components/bookmark/NoBookMark.tsx
@@ -1,13 +1,14 @@
+import Logo from '../../assets/images/pinkLogo.svg?react';
+
 export default function NoBookMark() {
-  return (
-    <div className="flex flex-col justify-center items-center translate-y-1/2">
-      <img src="./logo.svg" />
-      <div className="text-center text-2xl font-point">
-        북마크가 없습니다.
-        <br />
-        <span className="font-point text-primary">나만의 푸드트럭</span>을
-        추가하세요!
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col gap-y-2 justify-center items-center">
+            <Logo width={130} height={130} />
+            <div className="text-center text-2xl sm:text-3xl font-point">
+                북마크가 없습니다.
+                <br />
+                <span className="font-point text-primary">나만의 푸드트럭</span>을 추가하세요!
+            </div>
+        </div>
+    );
 }
