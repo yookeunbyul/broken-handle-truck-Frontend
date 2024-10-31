b93999c90082fcf8ca3742347078d9bf1ba23cd8
ppyom <ouob77@kakao.com>
2024-10-31 11:22:02 +0900

    feat: 내 주변 가게 불러와서 화면에 출력

--- src/pages/MapPage.tsx
+++ src/pages/MapPage.tsx
@@ -1,80 +1,82 @@
-import { useEffect, useRef, useState } from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
-import Toggle from '../components/Toggle';
-import Search from '../components/map/Search';
-import MyLocation from '../components/map/MyLocation';
-import Card from '../components/Card';
-import { useMyLocation } from '../hooks/useMyLocation';
+import { useEffect, useRef, useState } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import Toggle from "../components/Toggle";
+import Search from "../components/map/Search";
+import MyLocation from "../components/map/MyLocation";
+import Card from "../components/Card";
+import { useMyLocation } from "../hooks/useMyLocation";
+import { IStore } from "../types/store";
+import { getStoreList } from "../apis/store.ts";
 
-import truckData from '../mocks/truckDatas.json';
+type Coordinates = [number, number];
 
-interface Coordinates {
-    lat: number;
-    lng: number;
-}
-
-export interface MockMarker {
-
-  id: number;
-  name: string;
-  category: string;
-  coordinates: Coordinates;
-  visited: number;
-  isContinue: boolean;
-
+export interface Marker extends IStore {
+  visited?: number;
+  isContinue?: boolean;
 }
 
 export default function MapPage() {
-    // 마커 클릭 여부
-    const [clickMarker, setClickMarker] = useState<MockMarker | null>(null);
+  // 마커 클릭 여부
+  const [clickMarker, setClickMarker] = useState<Marker | null>(null);
 
-    // 초기 위치 설정
-    const [mapCenter, setMapCenter] = useState<Coordinates>({
-        lat: 37.5563,
-        lng: 126.99581,
+  // 초기 위치 설정
+  const [mapCenter, setMapCenter] = useState<Coordinates>([126.99581, 37.5563]);
+
+  const [storeList, setStoreList] = useState<IStore[]>([]);
+
+  const mapRef = useRef<kakao.maps.Map | null>(null);
+
+  // useMyLocation 훅으로 현재 위치 받아오는 함수
+  const { myLocation } = useMyLocation(({ latitude, longitude }) => {
+    setMapCenter([longitude, latitude]);
+  });
+
+  const handleLocationChange = (coords: {
+    latitude: number;
+    longitude: number;
+  }) => {
+    setMapCenter([coords.longitude, coords.latitude]);
+  };
+
+  const updateStoreList = (lat: number, lon: number) => {
+    getStoreList({ lat, lon }).then((data) => {
+      setStoreList(data.stores);
     });
-    const mapRef = useRef<kakao.maps.Map | null>(null);
+  };
 
-    // useMyLocation 훅으로 현재 위치 받아오는 함수
-    const { myLocation } = useMyLocation(({ latitude, longitude }) => {
-        setMapCenter({ lat: latitude, lng: longitude });
-    });
+  // 처음 로드될 때 내 위치로 이동
+  useEffect(() => {
+    myLocation();
+  }, []);
 
-    const handleLocationChange = (coords: { latitude: number; longitude: number }) => {
-        setMapCenter({ lat: coords.latitude, lng: coords.longitude });
-    };
+  // mapCenter가 업데이트될 때마다 지도 중심 이동
+  useEffect(() => {
+    if (mapRef.current) {
+      const [lon, lat] = mapCenter;
+      mapRef.current.setCenter(new window.kakao.maps.LatLng(lat, lon));
+      updateStoreList(lat, lon);
+    }
+  }, [mapCenter]);
 
-    // 처음 로드될 때 내 위치로 이동
-    useEffect(() => {
-        myLocation();
-    }, []);
-
-    // mapCenter가 업데이트될 때마다 지도 중심 이동
-    useEffect(() => {
-        if (mapRef.current) {
-            mapRef.current.setCenter(new window.kakao.maps.LatLng(mapCenter.lat, mapCenter.lng));
-        }
-    }, [mapCenter]);
-
-    return (
-        <>
-            <Map
-                center={mapCenter}
-                ref={mapRef}
-                className="w-full h-full relative"
-                level={4}
-                onClick={() => setClickMarker(null)}
-            >
-                {truckData.map((data) => (
-                    <MapMarker
-                        key={`${data.name}-${data.coordinates}`}
-                        position={data.coordinates}
-                        title={data.name}
-                        onClick={() => {
-                            setClickMarker(data);
-                        }}
-                    />
-                ))}
+  return (
+    <>
+      <Map
+        center={{ lat: mapCenter[1], lng: mapCenter[0] }}
+        ref={mapRef}
+        className="w-full h-full relative"
+        level={4}
+        onClick={() => setClickMarker(null)}
+      >
+        {storeList.map((data) => (
+          <MapMarker
+            key={`${data.name}-${data._id}`}
+            position={{ lat: data.coordinates[1], lng: data.coordinates[0] }}
+            title={data.name}
+            onClick={() => {
+              setClickMarker(data);
+            }}
+          />
+        ))}
 
                 <Search />
                 {/* 영업 여부 토글은 로그인 여부에 따라서 달라짐 */}
@@ -91,4 +93,26 @@ export default function MapPage() {
         </>
     );
 
+        <Search />
+        {/* 영업 여부 토글은 로그인 여부에 따라서 달라짐 */}
+        <div className="absolute flex items-center justify-between bottom-24 z-10 w-9/12 left-1/2 -translate-x-1/2">
+          <MyLocation setMapCenter={handleLocationChange} />
+          <Toggle text={{ on: "영업중", off: "영업 종료" }} />
+        </div>
+        {clickMarker ? (
+          <div className="absolute inset-x-1/2 -translate-x-1/2 bottom-24 z-10 w-9/12">
+            <Card
+              isOpen={false}
+              info={{
+                category: clickMarker.category,
+                name: clickMarker.name,
+                visited: 0,
+              }}
+              bg="black"
+            />
+          </div>
+        ) : null}
+      </Map>
+    </>
+  );
 }
