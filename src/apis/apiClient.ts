f9a2d7cd7035865b774d7fb711351229573b8204
yook <si89336@naver.com>
2024-10-31 14:08:44 +0900

    setting: post 처리 추가

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,23 +1,23 @@
-import axios from "axios";
+import axios from 'axios';
 
 const service = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
-  withCredentials: true,
-  timeout: 10000,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/`,
+    withCredentials: true,
+    timeout: 10000,
 });
 
 service.interceptors.response.use(null, (error) => {
-  return error.response;
+    return error.response;
 });
 
 export const http = {
-  get: function get<T, P = undefined>(url: string, params?: P): Promise<T> {
-    return service.get(url, { params }).then((res) => res.data);
-  },
-  post: function post<T, D = undefined>(url: string, data?: D): Promise<T> {
-    return service.post(url, data && { ...data });
-  },
-  delete: function remove<T>(url: string): Promise<T> {
-    return service.delete(url);
-  },
+    get: function get<T, P = undefined>(url: string, params?: P): Promise<T> {
+        return service.get(url, { params }).then((res) => res.data);
+    },
+    post: function post<T, D = undefined>(url: string, data?: D): Promise<T> {
+        return service.post(url, data && { ...data }).then((res) => res.data);
+    },
+    delete: function remove<T>(url: string): Promise<T> {
+        return service.delete(url);
+    },
 };
