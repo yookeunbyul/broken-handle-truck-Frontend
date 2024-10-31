78e926fcbe63aaab4ab67fa333770679b97bd955
ppyom <ouob77@kakao.com>
2024-10-31 15:50:52 +0900

    design: Detail 페이지 path 변경에 따른 layout 수정

--- src/layouts/TopBar.tsx
+++ src/layouts/TopBar.tsx
@@ -1,52 +1,69 @@
-import LeftArrowIcon from '../assets/images/leftArrow.svg?react';
-import { useLocation } from 'react-router-dom';
-import BookMarkButton from '../components/BookMarkButton';
-import useFadeNavigate from '../hooks/useFadeNavigate';
-import Notification from '../assets/images/noti.svg?react';
+import LeftArrowIcon from "../assets/images/leftArrow.svg?react";
+import { useLocation } from "react-router-dom";
+import BookMarkButton from "../components/BookMarkButton";
+import useFadeNavigate from "../hooks/useFadeNavigate";
+import Notification from "../assets/images/noti.svg?react";
+import { useMemo } from "react";
 
 type TopBarProps = {
-    title?: string;
+  title?: string;
 };
 
 export default function TopBar({ title }: TopBarProps) {
-    const navigate = useFadeNavigate();
-    const { pathname } = useLocation();
+  const navigate = useFadeNavigate();
+  const { pathname: _pathname } = useLocation();
 
-    const onClickLeft = () => {
-        if (document.startViewTransition) {
-            document.startViewTransition(() => {
-                navigate(-1);
-            });
-        } else {
-            navigate(-1);
-        }
-    };
+  const pathname = useMemo(() => `/${_pathname.split("/")[1]}`, [_pathname]);
 
-    const onClickRight = () => {};
+  const onClickLeft = () => {
+    if (document.startViewTransition) {
+      document.startViewTransition(() => {
+        navigate(-1);
+      });
+    } else {
+      navigate(-1);
+    }
+  };
 
-    return (
-        <div className="flex w-full bg-primary justify-center items-center px-4 py-5 sticky top-0 z-10">
-            <div className="w-[calc(100%-50px)] sm:w-[calc(100%-100px)] flex items-center">
-                <div className="flex-1 flex items-center">
-                    {(pathname === '/detail' || pathname === '/register' || pathname === '/notification') && (
-                        <div onClick={onClickLeft}>
-                            <LeftArrowIcon width={25} height={25} className="fill-white cursor-pointer" />
-                        </div>
-                    )}
-                </div>
-                <p className="flex-1 text-center text-lg text-white font-bold">{title}</p>
-                <div className="flex-1 flex items-center justify-end">
-                    {pathname === '/detail' && <BookMarkButton isBookmarked={false} onClick={onClickRight} size={30} />}
-                    {pathname === '/bookmark' && (
-                        <Notification
-                            width={25}
-                            height={25}
-                            className="cursor-pointer"
-                            onClick={() => navigate(`/notification`)}
-                        />
-                    )}
-                </div>
+  const onClickRight = () => {};
+
+  return (
+    <div className="flex w-full bg-primary justify-center items-center px-4 py-5 sticky top-0 z-10">
+      <div className="w-[calc(100%-50px)] sm:w-[calc(100%-100px)] flex items-center">
+        <div className="flex-1 flex items-center">
+          {(pathname === "/detail" ||
+            pathname === "/register" ||
+            pathname === "/notification") && (
+            <div onClick={onClickLeft}>
+              <LeftArrowIcon
+                width={25}
+                height={25}
+                className="fill-white cursor-pointer"
+              />
             </div>
+          )}
         </div>
-    );
+        <p className="flex-1 text-center text-lg text-white font-bold">
+          {title}
+        </p>
+        <div className="flex-1 flex items-center justify-end">
+          {pathname === "/detail" && (
+            <BookMarkButton
+              isBookmarked={false}
+              onClick={onClickRight}
+              size={30}
+            />
+          )}
+          {pathname === "/bookmark" && (
+            <Notification
+              width={25}
+              height={25}
+              className="cursor-pointer"
+              onClick={() => navigate(`/notification`)}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
