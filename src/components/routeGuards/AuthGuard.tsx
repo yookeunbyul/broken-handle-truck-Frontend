c0e9d986673201825566754b4a1784382292772a
Kim-HyounSuk <kkoor586@naver.com>
2024-11-01 17:15:36 +0900

    Refactor: [routeGuards] 컴포넌트 재렌더링 개선을 위한 로직 수정

--- src/components/routeGuards/AuthGuard.tsx
+++ src/components/routeGuards/AuthGuard.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import useUserStore from '../../store/userStore';
 import useFadeNavigate from '../../hooks/useFadeNavigate';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import useAuthValidation from '../../hooks/useAuthValidation';
 
 export default function AuthGaurd(): React.ReactElement {
 	const { setUser, user } = useUserStore();
 	const navigate = useFadeNavigate();
+	const location = useLocation();
 	const { data, isLoading } = useAuthValidation();
 
 	useEffect(() => {
@@ -14,12 +15,19 @@ export default function AuthGaurd(): React.ReactElement {
 			if (!isLoading) {
 				if (data?.user) {
 					setUser(data.user);
+					if (location.pathname === '/login' || location.pathname === '/signup') {
+						navigate('/map');
+					}
 				} else {
-					navigate('/login');
+					if (!user) {
+						navigate('/login');
+					}
 				}
 			}
 		}
-	}, [isLoading, user, navigate, setUser, data?.user]);
+		
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isLoading, data?.user]);
 
 	return <Outlet />;
 }
