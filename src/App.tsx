89e28bdb6a1af8b79fc844648418749dc01f3f74
yook <si89336@naver.com>
2024-11-01 18:05:04 +0900

    feat: toast message 적용

--- src/App.tsx
+++ src/App.tsx
@@ -13,30 +13,33 @@ import MyPage from './pages/MyPage.tsx';
 import MainGuard from './components/routeGuards/MainGuard.tsx';
 import AuthGaurd from './components/routeGuards/AuthGuard.tsx';
 import PublicGuard from './components/routeGuards/PublicGuard.tsx';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<RootLayout />}>
-					<Route index={true} element={<MainGuard />} />
-					<Route element={<PublicGuard />}>
-						<Route path='/login' element={<LoginPage />} />
-						<Route path='/signup' element={<SignUpPage />} />
-					</Route>
-					<Route element={<AuthGaurd />}>
-						<Route path='/map' element={<MapPage />} />
-						<Route path='/bookmark' element={<BookMarkPage />} />
-						<Route path='/detail/:id' element={<DetailPage />} />
-						<Route path='/my-truck' element={<MyTruckPage />} />
-						<Route path='/register' element={<RegisterPage />} />
-						<Route path='/notification' element={<NotificationPage />} />
-						<Route path='/my-page' element={<MyPage />} />
-					</Route>
-				</Route>
-			</Routes>
-		</Router>
-	);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<RootLayout />}>
+                    <Route index={true} element={<MainGuard />} />
+                    <Route element={<PublicGuard />}>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/signup" element={<SignUpPage />} />
+                    </Route>
+                    <Route element={<AuthGaurd />}>
+                        <Route path="/map" element={<MapPage />} />
+                        <Route path="/bookmark" element={<BookMarkPage />} />
+                        <Route path="/detail/:id" element={<DetailPage />} />
+                        <Route path="/my-truck" element={<MyTruckPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/notification" element={<NotificationPage />} />
+                        <Route path="/my-page" element={<MyPage />} />
+                    </Route>
+                </Route>
+            </Routes>
+            <ToastContainer position="bottom-center" />
+        </Router>
+    );
 }
 
 export default App;
