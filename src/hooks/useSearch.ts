54392cba846f6c6bd0a4d1534cb57ee0fb38a27a
ppyom <ouob77@kakao.com>
2024-11-01 16:24:10 +0900

    feat: 검색 기능

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import useStoresStore from "../store/storesStore.ts";
 
 export const useSearch = () => {
   const [searchTerm, setSearchTerm] = useState<string>("");
-
+  const { setSearchTerm: setKeyword } = useStoresStore();
   const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     // console.log(e.target.value); // input change 값 확인용
     setSearchTerm(e.target.value);
@@ -11,12 +12,14 @@ export const useSearch = () => {
   const clickHandler = () => {
     // 검색 아이콘 클릭시 input 데이터 값 확인용
     console.log("검색 아이콘 클릭", searchTerm);
+    setKeyword(searchTerm);
   };
 
   const searchHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       // 엔터 키 입력 시 input 데이터 값 확인용
       console.log("엔터 키 결과: ", searchTerm);
+      setKeyword(searchTerm);
     }
   };
 
