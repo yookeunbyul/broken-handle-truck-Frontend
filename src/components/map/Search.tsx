900b444db4fee7689c67feac58cd84bd1ed09764
JunYoung <whwnsdud011@naver.com>
2024-11-05 23:11:17 +0900

    design: 검색 아이콘 hover pointer 삭제

--- src/components/map/Search.tsx
+++ src/components/map/Search.tsx
@@ -7,7 +7,7 @@ export default function Search() {
 
   return (
     <div className="w-[calc(100%-50px)] sm:w-[calc(100%-200px)] mx-auto flex absolute items-center border border-category rounded-lg gap-2 top-6 left-1/2 -translate-x-1/2 z-10 bg-white px-2 py-3">
-      <SearchIcon className="w-6 h-6 cursor-pointer" />
+      <SearchIcon className="w-6 h-6" />
       <input
         className="w-full text-base border-none outline-none tracking-tighter"
         type="text"
