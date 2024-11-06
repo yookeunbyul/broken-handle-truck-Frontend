9d452e21ba834c709ea2f312437417d1dfeead02
Kim-HyounSuk <kkoor586@naver.com>
2024-11-06 17:25:47 +0900

    fix: Mypage의 리뷰 컴포넌트 수정(상세 페이지 이동)

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,59 +1,59 @@
-import { toast } from "react-toastify";
-import { deleteComment } from "../apis/comment.ts";
-import useUserStore from "../store/userStore.ts";
-import Delete from "../assets/images/delete.svg?react";
-import type {
-  QueryObserverResult,
-  RefetchOptions,
-} from "@tanstack/react-query";
+import { toast } from 'react-toastify';
+import { deleteComment } from '../apis/comment.ts';
+import useUserStore from '../store/userStore.ts';
+import Delete from '../assets/images/delete.svg?react';
+import type { QueryObserverResult, RefetchOptions } from '@tanstack/react-query';
+import { useLocation } from 'react-router-dom';
+import useFadeNavigate from '../hooks/useFadeNavigate.ts';
 
 interface CommentProps {
-  id: string;
-  authorId: string;
-  refetch: (
-    options?: RefetchOptions,
-  ) => Promise<QueryObserverResult<{ comments: unknown[] }, unknown>>;
-  name: string;
-  content: string;
-  createdAt: string;
+	id: string;
+	authorId: string;
+	refetch: (options?: RefetchOptions) => Promise<QueryObserverResult<{ comments: unknown[] }, unknown>>;
+	name: string;
+	content: string;
+	createdAt: string;
+	storeId: string;
 }
 
-export default function Comment({
-  id,
-  authorId,
-  refetch,
-  name,
-  createdAt,
-  content,
-}: CommentProps) {
-  const { user } = useUserStore();
+export default function Comment({ id, authorId, refetch, name, createdAt, content, storeId }: CommentProps) {
+	const { user } = useUserStore();
+	const location = useLocation();
+	const navigate = useFadeNavigate();
 
-  const handleDeleteComment = () => {
-    deleteComment({ commentId: id }).then((data) => {
-      if (data.msg === "ok") {
-        refetch().then(() => {
-          toast.success("삭제되었습니다.");
-        });
-      } else {
-        toast.error(data.msg);
-      }
-    });
-  };
+	const handleDeleteComment = () => {
+		deleteComment({ commentId: id }).then((data) => {
+			if (data.msg === 'ok') {
+				refetch().then(() => {
+					toast.success('삭제되었습니다.');
+				});
+			} else {
+				toast.error(data.msg);
+			}
+		});
+	};
 
-  return (
-    <div className="bg-white tracking-tighter border-b-1 border-comment">
-      <div className="flex justify-between py-3 text-xs ">
-        <div className="font-bold text-black">{name}</div>
-        <div className="flex gap-x-2 align-middle">
-          <div className="text-category">{createdAt}</div>
-          {user?._id === authorId && (
-            <button onClick={handleDeleteComment}>
-              <Delete width={15} height={15} />
-            </button>
-          )}
-        </div>
-      </div>
-      <div className="py-5 text-sm text-black text-left">{content}</div>
-    </div>
-  );
+	return (
+		<div className='bg-white tracking-tighter border-b-1 border-comment'>
+			<div className='flex justify-between py-3 text-xs items-center'>
+				<div
+					onClick={() => location.pathname === '/my-page' && navigate(`/detail/${storeId}`)}
+					className={`font-bold text-black text-base ${
+						location.pathname === '/my-page' && 'cursor-pointer hover:text-primary'
+					}`}
+				>
+					{name}
+				</div>
+				<div className='flex gap-x-2 align-middle'>
+					<div className='text-category'>{createdAt}</div>
+					{user?._id === authorId && (
+						<button onClick={handleDeleteComment}>
+							<Delete width={15} height={15} />
+						</button>
+					)}
+				</div>
+			</div>
+			<div className='py-5 text-sm text-black text-left break-words whitespace-normal'>{content}</div>
+		</div>
+	);
 }
