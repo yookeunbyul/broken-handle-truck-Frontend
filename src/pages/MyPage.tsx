18d97f2e8cef44577a0b45e4dfc4545b0c9eb2b2
yook <si89336@naver.com>
2024-11-06 09:35:41 +0900

    fix: 로그아웃하면서 전환되는 문제 수정

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -83,98 +83,108 @@ export default function MyPage() {
     }, [userInfo]);
 
     return (
-        <div className="px-8 sm:px-0 max-w-lg mx-auto h-full flex flex-col gap-8 sm:gap-12 pt-[50px] sm:pt-[60px]">
-            <div className="flex justify-between items-center gap-4">
-                <div className="flex justify-center">
-                    <div className="rounded-full w-24 h-24 bg-review">
-                        {userInfo?.thumnail === '' ? (
-                            <LogoIcon
-                                width={96}
-                                height={96}
-                                className="border-1 rounded-full border-category p-2 fill-logo-violet bg-white"
-                            />
-                        ) : (
-                            <img src={userInfo?.thumnail} className="rounded-full w-24 h-24 border-1 border-category" />
-                        )}
-                    </div>
-                </div>
-                <div className="flex-1 flex items-center gap-1 relative">
-                    <div className="flex flex-col gap-y-1">
-                        <div className="">
-                            <span className="tracking-tighter text-lg pl-1">{nickname}</span>
-                            <input
-                                ref={nicknameRef}
-                                className="outline-none absolute left-0 bg-transparent w-full tracking-tighter text-lg pl-1"
-                                type="text"
-                                value={nickname}
-                                onChange={({ target }) => setNickname(target.value)}
-                                disabled={!isEditMode}
-                            />
-                            <button className="relative" onClick={handleEditNickname}>
-                                {isEditMode ? (
-                                    <CheckIcon width={16} height={16} />
+        <>
+            {!userInfo ? null : (
+                <div className="px-8 sm:px-0 max-w-lg mx-auto h-full flex flex-col gap-8 sm:gap-12 pt-[50px] sm:pt-[60px]">
+                    <div className="flex justify-between items-center gap-4">
+                        <div className="flex justify-center">
+                            <div className="rounded-full w-24 h-24 bg-review">
+                                {!userInfo?.thumnail ? (
+                                    <LogoIcon
+                                        width={96}
+                                        height={96}
+                                        className="border-1 rounded-full border-category p-2 fill-logo-violet bg-white"
+                                    />
                                 ) : (
-                                    <EditIcon
-                                        className="duration-300 stroke-gray hover:stroke-primary"
-                                        width={16}
-                                        height={16}
+                                    <img
+                                        src={userInfo?.thumnail}
+                                        className="rounded-full w-24 h-24 border-1 border-category"
                                     />
                                 )}
-                            </button>
+                            </div>
                         </div>
-                        <div className="text-base text-category tracking-tighter pl-1">
-                            {userInfo?.role === 'owner' ? '푸드트럭 사장님' : '푸드트럭 발견가'}
+                        <div className="flex-1 flex items-center gap-1 relative">
+                            <div className="flex flex-col gap-y-1">
+                                <div className="">
+                                    <span className="tracking-tighter text-lg pl-1">{nickname}</span>
+                                    <input
+                                        ref={nicknameRef}
+                                        className="outline-none absolute left-0 bg-transparent w-full tracking-tighter text-lg pl-1"
+                                        type="text"
+                                        value={nickname}
+                                        onChange={({ target }) => setNickname(target.value)}
+                                        disabled={!isEditMode}
+                                    />
+                                    <button className="relative" onClick={handleEditNickname}>
+                                        {isEditMode ? (
+                                            <CheckIcon width={16} height={16} />
+                                        ) : (
+                                            <EditIcon
+                                                className="duration-300 stroke-gray hover:stroke-primary"
+                                                width={16}
+                                                height={16}
+                                            />
+                                        )}
+                                    </button>
+                                </div>
+                                <div className="text-base text-category tracking-tighter pl-1">
+                                    {userInfo?.role === 'owner' ? '푸드트럭 사장님' : '푸드트럭 발견가'}
+                                </div>
+                            </div>
                         </div>
+                        <button
+                            className="border-1 text-sm border-category text-category px-4 py-1.5 rounded-lg duration-300 hover:border-primary hover:text-primary"
+                            onClick={handleLogoutClick}
+                        >
+                            로그아웃
+                        </button>
                     </div>
-                </div>
-                <button
-                    className="border-1 text-sm border-category text-category px-4 py-1.5 rounded-lg duration-300 hover:border-primary hover:text-primary"
-                    onClick={handleLogoutClick}
-                >
-                    로그아웃
-                </button>
-            </div>
-            <button
-                className="text-xl text-primary border-1 border-primary font-bold tracking-tight w-full py-6 rounded-lg drop-shadow-lg bg-white"
-                onClick={() => navigate(`/my-truck`)}
-            >
-                <div className="flex gap-x-2 justify-center items-center">
-                    내 가게 관리하기
-                    <Truck width={23} height={23} className="pt-1" />
-                </div>
-            </button>
-            <div className="flex-1">
-                <div className="flex justify-start items-center gap-x-1 text-base mb-3 pt-2">
-                    <MessageSquareIcon width={16} height={16} />
-                    <span className="tracking-tight">
-                        내가 남긴 리뷰 (<strong className="text-primary">{data?.comments.length}</strong>)
-                    </span>
-                </div>
-                {(isLoading || !data?.comments) && <Loading />}
-                {data?.comments &&
-                    (data.comments.length === 0 ? (
-                        <div className="h-full">
-                            <NoReview />
+                    <button
+                        className="text-xl text-primary border-1 border-primary font-bold tracking-tight w-full py-6 rounded-lg drop-shadow-lg bg-white"
+                        onClick={() => navigate(`/my-truck`)}
+                    >
+                        <div className="flex gap-x-2 justify-center items-center">
+                            내 가게 관리하기
+                            <Truck width={23} height={23} className="pt-1" />
                         </div>
-                    ) : (
-                        <div>
-                            {data.comments.map((comment) => (
-                                <Comment
-                                    key={`my_comment_${comment._id}`}
-                                    id={comment._id}
-                                    content={comment.content}
-                                    name={comment.storeId.name}
-                                    authorId={comment.authorId}
-                                    createdAt={comment.createdAt}
-                                    refetch={refetch}
-                                />
+                    </button>
+                    <div className="flex-1">
+                        <div className="flex justify-start items-center gap-x-1 text-base mb-3 pt-2">
+                            <MessageSquareIcon width={16} height={16} />
+                            <span className="tracking-tight">
+                                내가 남긴 리뷰 (<strong className="text-primary">{data?.comments.length}</strong>)
+                            </span>
+                        </div>
+                        {(isLoading || !data?.comments) && <Loading />}
+                        {data?.comments &&
+                            (data.comments.length === 0 ? (
+                                <div className="h-full">
+                                    <NoReview />
+                                </div>
+                            ) : (
+                                <div>
+                                    {data.comments.map((comment) => (
+                                        <Comment
+                                            key={`my_comment_${comment._id}`}
+                                            id={comment._id}
+                                            content={comment.content}
+                                            name={comment.storeId.name}
+                                            authorId={comment.authorId}
+                                            createdAt={comment.createdAt}
+                                            refetch={refetch}
+                                        />
+                                    ))}
+                                </div>
                             ))}
-                        </div>
-                    ))}
-            </div>
-            <button className="self-center text-category underline text-sm pt-6 pb-4" onClick={handleAccountDeletion}>
-                탈퇴하기
-            </button>
-        </div>
+                    </div>
+                    <button
+                        className="self-center text-category underline text-sm pt-6 pb-4"
+                        onClick={handleAccountDeletion}
+                    >
+                        탈퇴하기
+                    </button>
+                </div>
+            )}
+        </>
     );
 }
